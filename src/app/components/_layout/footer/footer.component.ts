bd7dd9533f1dd381082a53c1ec320caea9ba1209
Sefy <mrsefy@gmail.com>
2024-12-10 23:08:13 +0100

    About : add contact section, & still other text changes ...

--- src/app/components/_layout/footer/footer.component.ts
+++ src/app/components/_layout/footer/footer.component.ts
@@ -1,13 +1,11 @@
 import { Component } from '@angular/core';
 import { MatToolbar } from "@angular/material/toolbar";
-import { RouterLink } from "@angular/router";
 import { IconComponent } from "@app/components/_global/icon/icon.component";
 
 @Component({
   selector: 'app-footer',
   imports: [
     MatToolbar,
-    RouterLink,
     IconComponent
   ],
   templateUrl: './footer.component.html',
--- src/app/pages/about/about.component.ts
+++ src/app/pages/about/about.component.ts
@@ -3,6 +3,8 @@ import { FooterComponent } from "@app/components/_layout/footer/footer.component
 import { HeroBannerComponent } from "@app/components/hero-banner/hero-banner.component";
 import { PrezBlockComponent } from "@app/components/_global/prez-block/prez-block.component";
 import { LoaderComponent } from "@app/components/_global/loader/loader.component";
+import { IconComponent } from "@app/components/_global/icon/icon.component";
+import { MatButton } from "@angular/material/button";
 
 @Component({
   selector: 'app-about',
@@ -10,11 +12,27 @@ import { LoaderComponent } from "@app/components/_global/loader/loader.component
     FooterComponent,
     HeroBannerComponent,
     PrezBlockComponent,
-    LoaderComponent
+    LoaderComponent,
+    IconComponent,
+    MatButton
   ],
   templateUrl: './about.component.html',
   styleUrl: './about.component.scss'
 })
 export class AboutComponent {
   @HostBinding('class.page-content') isPageContent = true;
+
+  externalLinks = {
+    discord: 'https://discord.gg/FrUZW4kQu6',
+    github: 'https://github.com/Sefy/democracia',
+    patreon: ''
+  };
+
+  openLink(type: 'github' | 'discord' | 'patreon') {
+    const url = this.externalLinks[type];
+
+    if (url) {
+      window.open(url, '_blank');
+    }
+  }
 }
